b79f918b75f2b3ba782fa86d679c0bfa02018db0
muhammadaliyev <muhammadaliyev>
2025-08-03 13:58:55 +0500

    muhammadaliyev

--- js/app.js
+++ js/app.js
@@ -6,6 +6,64 @@ while (idish !== 5) {
   idish = idish + 1;
   console.log("salom dunyo");
 }
+//uyga vazifa 1- masala
+let son;
+
+while (true) {
+  son = prompt("1 dan 999 gacha son kiriting:");
+  son = Number(son);
+
+  if (son >= 1 && son <= 999) {
+    break;
+  } else {
+    console.log("Iltimos, 1 dan 999 gacha son kiriting.");
+  }
+}
+
+let birlik = [
+  "",
+  "bir",
+  "ikki",
+  "uch",
+  "to'rt",
+  "besh",
+  "olti",
+  "yetti",
+  "sakkiz",
+  "to'qqiz",
+];
+let onlik = [
+  "",
+  "o'n",
+  "yigirma",
+  "o'ttiz",
+  "qirq",
+  "ellik",
+  "oltmish",
+  "yetmish",
+  "sakson",
+  "to'qson",
+];
+let yuzlik = [
+  "",
+  "bir yuz",
+  "ikki yuz",
+  "uch yuz",
+  "to'rt yuz",
+  "besh yuz",
+  "olti yuz",
+  "yetti yuz",
+  "sakkiz yuz",
+  "to'qqiz yuz",
+];
+
+let yuz = Math.floor(son / 100);
+let on = Math.floor((son % 100) / 10);
+let bir = son % 10;
+
+let natija = yuzlik[yuz] + " " + onlik[on] + " " + birlik[bir];
+
+console.log("Natija: " + natija.trim());
 
 //uyga vazifa 2- masala
 
